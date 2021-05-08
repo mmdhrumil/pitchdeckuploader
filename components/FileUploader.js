1aa0466b2d594404a9fc15d3237f8e62265e6607
Dhrumil Mehta <dhrumil.m@somaiya.edu>
2021-05-08 12:22:04 -0400

    Add upload button to handle uploading files events

--- components/FileUploader.js
+++ components/FileUploader.js
@@ -3,6 +3,9 @@ import { useDropzone } from 'react-dropzone'
 import { createClient } from '@supabase/supabase-js'
 import styles from './styles/FileUploader.module.css'
 
+
+var fileObject = null;
+
 async function connectSupabase(){
     const supabase = await createClient(process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
     return supabase
@@ -36,23 +39,36 @@ async function uploadData(acceptedFiles){
 
 }
 
+async function uploadFiles() {
+    console.log("sending: " + fileObject)
+    await uploadData(fileObject)
+
+    console.log("Dunzo")
+}
+async function setFileObject(acceptedFiles) {
+    fileObject = acceptedFiles;
+}
+
 function FileUploader() {
 
     const onDrop = useCallback(acceptedFiles => {
-        uploadData(acceptedFiles)
+        setFileObject(acceptedFiles)
     }, [])
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
 
     return (
-        <div {...getRootProps()} className={styles.mainContainer}>
-            <div className={styles.dropperContainer}>
+        <div className={styles.mainContainer}>
+            <div {...getRootProps()} className={styles.dropperContainer}>
                 <input {...getInputProps()} />
                 {
                     isDragActive ?
                     <p>Drop your pitch deck here.</p> :
                     <p>Drag and drop your pitch deck here or click to select files.</p>
                 }
+            </div>
+            <div className={styles.buttonContainer}>
+                <button className={styles.uploadButton} onClick={uploadFiles}>Upload</button>
             </div>
         </div>
     );
