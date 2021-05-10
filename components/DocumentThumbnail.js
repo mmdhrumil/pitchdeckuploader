98b446f5c939960a1da3b9b9f18354e0ab003278
Dhrumil Mehta <dhrumil.m@somaiya.edu>
2021-05-09 22:56:01 -0400

    Update styling for DocumentDisplay

--- components/DocumentThumbnail.js
+++ components/DocumentThumbnail.js
@@ -2,10 +2,16 @@ import { React, useState } from 'react';
 import styles from './styles/DocumentThumbnail.module.css'
 
 const DocumentThumbnail = (props) => {
+    var name = props.data.name;
+    
     return (
-        <div className = { styles.thumbnailContainer}>
-            <h3>{props.data.name[0]}</h3>
-            <p>{props.data.name.slice(0,9) + "..."}</p>
+        <div className = {styles.thumbnailContainer}>
+            <a href="#">
+                <div className = {styles.thumbnailImage}>{name[0].toUpperCase()}</div>
+                <div className = {styles.thumbnailText}>
+                <p>{props.data.name.slice(0,30)}</p>
+            </div>
+            </a>
         </div>
     );
 }
